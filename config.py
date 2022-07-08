a771b561efeb1820b15837727724af6602987525
Hossein <hosseinmokhtari5555@gmail.com>
2022-07-08 12:49:09 +0430

    Solving the problem of the program

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 from configparser import ConfigParser
 
+file = "config.ini"
+
+config = ConfigParser()
+
+read = config.read(file)
+
 
 def read_config(section, element):
-    file = "config.ini"
-
-    config = ConfigParser()
-
-    read = config.read(file)
-
     b = config[section][element]
-
     return b
