280a439a8ff36b41af32e0e0721ba8dd0b56ee42
Hossein <hosseinmokhtari5555@gmail.com>
2022-07-08 14:37:22 +0430

    AD-4 message file reading

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 from configparser import ConfigParser
 
 file = "config.ini"
-
 config = ConfigParser()
+config.read(file)
+env = config['env']['env']
+conf_file = 'config.' + env + '.ini'
+conf = ConfigParser()
+conf.read(conf_file)
 
-read = config.read(file)
 
-
-def read_config(section, element):
-    b = config[section][element]
+def read(section, element):
+    b = conf[section][element]
     return b
