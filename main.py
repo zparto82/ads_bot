9937f85b9511711b173f1cedb4bdbcc53fe6324a
Hossein <hosseinmokhtari5555@gmail.com>
2022-07-17 16:28:12 +0430

    AD-11

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import config
+import code_creator
 import msg
+import json
 from telethon.sync import TelegramClient,events
 from telethon.tl.types import PeerUser
 from telethon import Button
@@ -10,10 +12,15 @@ bot_token = config.read("telegram","bot_token")
 bot = TelegramClient('bot', api_id, api_hash,proxy=('socks5','127.0.0.1',1080))
 bot.start(bot_token=bot_token)
 
-
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
     z = event.message.peer_id.user_id
+    code = code_creator.code()
+    d = {
+        z : code
+    }
+    if z in d.keys():
+        await bot.send_message(z,d[z])
     print(z)
     print(event.message)
     keyboard = [
@@ -33,10 +40,15 @@ async def start(event):
 
     await bot.send_message(z,msg.read_msg('Introduction') , buttons=keyboard)
 
+@bot.on(events.NewMessage(pattern="code:*"))
+async def code(event):
+    print(event.message.peer_id)
 
 @bot.on(events.CallbackQuery())
 async def handler(event):
-        await event.respond("hi")
+    pass
+
+
 def main():
     """Start the bot."""
     bot.run_until_disconnected()
