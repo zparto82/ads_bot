2a059b458283f155d2b3ab4e198349ea3887a26f
Hossein <hosseinmokhtari5555@gmail.com>
2022-07-19 14:05:37 +0430

    AD-11

--- main.py
+++ main.py
@@ -11,35 +11,23 @@ bot_token = config.read("telegram","bot_token")
 
 bot = TelegramClient('bot', api_id, api_hash,proxy=('socks5','127.0.0.1',1080))
 bot.start(bot_token=bot_token)
-d = {
 
-}
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
-    z = event.message.peer_id.user_id
-    if z in d.keys():
-        await bot.send_message(z,d[z])
-    else:
-        z = event.message.peer_id.user_id
-        code = code_creator.code()
-        c = d[z] = code
-        await bot.send_message(z,d[z])
-        print(d)
-
-    print(z)
+    z = event.message.peer_id
     print(event.message)
     keyboard = [
         [
             Button.inline(str(msg.read_msg('connect')),b'1'),
-            Button.inline(str(msg.read_msg("create")), "2"),
+            Button.inline(str(msg.read_msg("create")), b"2"),
         ],
         [
-            Button.inline(msg.read_msg("show"), "3"),
-            Button.inline(msg.read_msg("settings"), "4")
+            Button.inline(msg.read_msg("show"), b"3"),
+            Button.inline(msg.read_msg("settings"), b"4")
         ],
         [
-            Button.inline(msg.read_msg("buy coins"), "5"),
-            Button.inline(msg.read_msg("help"), "6")
+            Button.inline(msg.read_msg("buy coins"), b"5"),
+            Button.inline(msg.read_msg("help"), b"6")
         ]
     ]
 
@@ -49,9 +37,22 @@ async def start(event):
 async def code(event):
     print(event.message.peer_id)
 
+d = {
+
+}
 @bot.on(events.CallbackQuery())
 async def handler(event):
-    pass
+    if event.data == b'1':
+        z = event.original_update.user_id
+        if z in d.keys():
+            await bot.send_message(z,d[z])
+        else:
+            z = event.original_update.user_id
+            code = code_creator.code()
+            c = d[z] = code
+            await bot.send_message(z,msg.read_msg('code'))
+            await bot.send_message(z,d[z])
+            print(d)
 
 
 def main():
